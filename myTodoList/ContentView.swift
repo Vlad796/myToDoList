2652cea13e3f713bf5b3fd8c20826f9426a24942
Vlad796 <gonevlad02@gmail.com>
2025-01-24 21:02:21 +0300

    Initial Commit

---
+++ myTodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myTodoList
+//
+//  Created by Влад Варламов on 24.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myTodoList/myTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTodoListApp.swift
+//  myTodoList
+//
+//  Created by Влад Варламов on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct myTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
